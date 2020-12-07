5859bfe56acee721d670d50ad960343301e4f5a1
Ryan-Henson <72321799+Ryan-Henson@users.noreply.github.com>
2020-12-07 16:46:58 -0600

    attempting to deploy

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hamburger", true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Burger", true)
